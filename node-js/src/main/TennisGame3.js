4a4f9c966feabaf6e94526f84d478358e1fb6387
Jerome Gonzalvo <jerome.gonzalvo@orangeandbronze.com>
2020-12-06 10:04:57 +0800

    refactor: added improvements to the project
    used functions instead of classes for the tennis games; removed unused libraries; added eslint
    config to easily find problems in the code; removed unused files

--- node-js/src/main/TennisGame3.js
+++ node-js/src/main/TennisGame3.js
@@ -1,32 +1,32 @@
-export default class TennisGame3 {
-    p2 = 0;
-    p1 = 0;
-    p1N = '';
-    p2N = '';
+const TennisGame3 = function (p1N, p2N) {
+    let p1 = 0;
+    let p2 = 0;
 
-    TennisGame3(p1N, p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
-    }
-
-    getScore() {
+    const getScore = function () {
         let s;
-        if (this.p1 < 4 && this.p2 < 4) {
+        if (p1 < 4 && p2 < 4) {
             const p = ["Love", "Fifteen", "Thirty", "Forty"];
-            s = p[this.p1];
-            return this.p1 == this.p2 ? `${s}-All` : `${s}-${p[this.p2]}`;
+            s = p[p1];
+            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
         } else {
-            if (this.p1 == this.p2)
+            if (p1 == p2)
                 return "Deuce";
-            s = this.p1 > this.p2 ? "player1" : "player2";
-            return ((this.p1 - this.p2) * (this.p1 - this.p2) == 1) ? `Advantage ${s}` : `Win for ${s}`; 
+            s = p1 > p2 ? p1N : p2N;
+            return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
 
-    wonPoint(playerName) {
-        if(playerName == "player1")
-            this.p1 += 1;
+    const wonPoint = function (playerName) {
+        if (playerName == "player1")
+            p1 += 1;
         else
-            this.p2 += 1;
+            p2 += 1;
+    }
+
+    return {
+        getScore,
+        wonPoint
     }
 }
+
+module.exports = TennisGame3;
